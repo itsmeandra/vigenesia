133039c5b98ab1d0bf3f88562d1221b4cd347be0
andra <andrajs237@gmail.com>
2026-05-11 02:07:46 +0700

    style: improve main screen design

--- vigenesia-mobile/lib/views/home/main_screen.dart
+++ vigenesia-mobile/lib/views/home/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vigenesia_mobile/views/home/add_motivasi_page.dart';
 import 'home_page.dart';
 import 'profile_page.dart';
 
@@ -9,20 +10,12 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
-  
-  // Daftar halaman yang bisa dikunjungi
-  final List<Widget> _pages = [
-    HomePage(),
-    ProfilePage(),
-  ];
+
+  final List<Widget> _pages = [HomePage(), AddMotivasiPage(), ProfilePage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ViGeNesia"),
-        centerTitle: true,
-      ),
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
@@ -31,9 +24,15 @@ class _MainScreenState extends State<MainScreen> {
             _currentIndex = index;
           });
         },
-        items: [
+        selectedItemColor: Color(0xFFD4840C),
+        unselectedItemColor: Colors.grey,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+        type: BottomNavigationBarType.fixed,
+        items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Beranda"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil Saya"),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "Post"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
         ],
       ),
     );
